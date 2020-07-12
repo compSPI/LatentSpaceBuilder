e6c1380ac1765c2a4a8d37f578b4de8b3ef2d84f
Deeban Ramalingam <rdeeban@gmail.com>
2020-07-11 22:28:02 -0700

    Install package dependencies when setup.py is run

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = [
+    'bokeh>=2.1.1',
+    'jupyterlab>=2.1.5',
+    'scikit-learn>=0.23.1',
+    'h5py>=2.10.0',
+    'numpy>=1.19.0',
+    'pyDiffMap>=0.2.0.1',
+    'setuptools>=49.1.2'
+]
+
 setuptools.setup(
     name="latent-space-builder", # Replace with your own username
     version="0.0.1",
@@ -13,6 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/compSPI/LatentSpaceBuilder",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
