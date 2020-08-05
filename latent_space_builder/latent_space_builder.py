255aeb8be350ea569f4e87211ba95e5a881bc14b
Deeban Ramalingam <deebanr@pslogin01.slac.stanford.edu>
2020-08-04 20:48:24 -0700

    Add option to take a random sample of the images from the HDF5 file. Recreate a dataset with an existing key but different shape.

--- latent_space_builder/latent_space_builder.py
+++ latent_space_builder/latent_space_builder.py
@@ -1,3 +1,7 @@
+import time
+
+import numpy as np
+
 from sklearn import preprocessing
 
 from sklearn.decomposition import PCA
@@ -8,36 +12,53 @@ import h5py as h5
 
 def principal_component_analysis(data, latent_dim):
     pca = PCA(n_components=latent_dim)
-    latent = pca.fit_transform(data)
-    return latent
+    tic = time.time()
+    latent_vectors = pca.fit_transform(data)
+    toc = time.time()
+    print("It takes {:.2f} seconds for PCA to complete.".format(toc-tic))
+    return latent_vectors
 
 def diffusion_map(data, latent_dim):
     dmap = DiffusionMap.from_sklearn(n_evecs=latent_dim)
-    latent = dmap.fit_transform(data)
-    return latent
+    tic = time.time()
+    latent_vectors = dmap.fit_transform(data)
+    toc = time.time()
+    print("It takes {:.2f} seconds for diffusion map to complete.".format(toc-tic))
+    return latent_vectors
 
-def build_latent_space(dataset_file, image_type, target_type, latent_method, latent_dim):    
-    with h5.File(dataset_file, 'r') as dataset_file_handle:
-        print(dataset_file_handle.keys())
-        images = dataset_file_handle[image_type][:]
-        targets = dataset_file_handle[target_type][:]
-        
-    n, h, w = images.shape    
+def build_latent_space(dataset_file, image_type, target_type, latent_method, latent_dim, dataset_size, random_sample_size=None): 
+    if random_sample_size is not None:
+        # Consider a random sample of the image data
+        random_idx = np.random.choice(dataset_size, random_sample_size, replace=False)
+        random_idx_sorted = np.sort(random_idx)
+
+        # Read the image data from the HDF5 file
+        with h5.File(dataset_file, 'r') as dataset_file_handle:
+            images = dataset_file_handle[image_type][random_idx_sorted]
     
+    else:
+        # Read the image data from the HDF5 file
+        with h5.File(dataset_file, 'r') as dataset_file_handle:
+            images = dataset_file_handle[image_type][:]
+    
+    # Get the shape of the image data
+    n, h, w = images.shape
+    
+    # Normalize image data to be between 0 and 1
     scaled_image_vectors = preprocessing.minmax_scale(images.reshape((n, h * w)), feature_range=(0, 1))
     
+    # Specify the method used to build the latent space
     if latent_method == "principal_component_analysis":
-        latent = principal_component_analysis(scaled_image_vectors, latent_dim)
+        latent_vectors = principal_component_analysis(scaled_image_vectors, latent_dim)
     elif latent_method == "diffusion_map":
-        latent = diffusion_map(scaled_image_vectors, latent_dim)
+        latent_vectors = diffusion_map(scaled_image_vectors, latent_dim)
     else:
         raise Exception("Unrecognized latent method. Please choose from: principal_component_analysis, diffusion_map")
     
-    latent_shape = latent.shape
+    # Write the latent vectors to the HDF5 file
+    latent_vectors_shape = latent_vectors.shape
     with h5.File(dataset_file, 'a') as dataset_file_handle:
         if latent_method in dataset_file_handle:
-            dset_latent = dataset_file_handle[latent_method]
-        else:
-            dset_latent = dataset_file_handle.create_dataset(latent_method, latent_shape, dtype='f')
+            del dataset_file_handle[latent_method]
         
-        dset_latent[...] = latent
+        dataset_file_handle.create_dataset(latent_method, latent_vectors_shape, dtype='f', data=latent_vectors)
