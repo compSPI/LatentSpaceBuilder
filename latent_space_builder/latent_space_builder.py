74ee0aecda2ccd98c6340da1a5994d8bc139e8ef
Deeban Ramalingam <deebanr@pslogin02.slac.stanford.edu>
2020-08-05 11:19:22 -0700

    Add incremental pca to notebook and source code. Clean source code.

--- latent_space_builder/latent_space_builder.py
+++ latent_space_builder/latent_space_builder.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from sklearn import preprocessing
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, IncrementalPCA
 from pydiffmap.diffusion_map import DiffusionMap
 
 import h5py as h5
@@ -13,9 +13,11 @@ import h5py as h5
 def build_latent_space(dataset_file, image_type, target_type, latent_method, latent_dim, dataset_size, training_set_size, batch_size):
     # Check if batch_size divides dataset_size
     if dataset_size % batch_size != 0:
-        raise Exception("batch_size {} must divide {}".format(batch_size, dataset_size))
+        raise Exception("batch_size {} must divide dataset_size {}".format(batch_size, dataset_size))
     
-    # Read the image data from the HDF5 file
+    n_batches = dataset_size // batch_size
+    
+    # Read the image data from the dataset
     with h5.File(dataset_file, 'a') as dataset_file_handle:
         # Get handle to images
         images_handle = dataset_file_handle[image_type]
@@ -24,9 +26,10 @@ def build_latent_space(dataset_file, image_type, target_type, latent_method, lat
         h, w = images_handle[0].shape
 
         # Define a min-max rescaler to scale images between 0 and 1
+        # Adapted from: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.minmax_scale.html
         max_value = np.finfo(np.float).min
         min_value = np.finfo(np.float).max
-        n_batches = dataset_size // batch_size
+        
         for i in range(n_batches):
             image_batch = images_handle[i * batch_size : (i + 1) * batch_size]
             max_value = max(max_value, np.max(image_batch))
@@ -45,7 +48,7 @@ def build_latent_space(dataset_file, image_type, target_type, latent_method, lat
         # Rescale the training set
         training_set_rescaled = minmax_rescale(training_set_vectors)
         
-        # Apply latent (dimensionality reduction) method
+        # Fit the latent (dimensionality reduction) method to the training set
         latent_model = None
         if latent_method == "principal_component_analysis":
             # Fit PCA to the rescaled training set
@@ -54,6 +57,7 @@ def build_latent_space(dataset_file, image_type, target_type, latent_method, lat
             latent_model.fit(training_set_rescaled)
             toc = time.time()
             print("It takes {:.2f} seconds for PCA to fit to the training set of shape {}.".format(toc-tic, training_set_rescaled.shape))
+        
         elif latent_method == "diffusion_map":
             # Fit Diffusion Map to the rescaled training set
             latent_model = DiffusionMap.from_sklearn(n_evecs=latent_dim)
@@ -61,6 +65,31 @@ def build_latent_space(dataset_file, image_type, target_type, latent_method, lat
             latent_model.fit(training_set_rescaled)
             toc = time.time()
             print("It takes {:.2f} seconds for Diffusion Map to fit to the training set of shape {}.".format(toc-tic, training_set_rescaled.shape))
+        
+        elif latent_method == "incremental_principal_component_analysis":
+            # Check if batch_size divides training_set_size
+            if training_set_size % batch_size != 0:
+                raise Exception("batch_size {} must divide training_set_size {}".format(batch_size, training_set_size))
+
+            n_training_batches = training_set_size // batch_size
+
+            # https://stackoverflow.com/questions/31428581/incremental-pca-on-big-data
+            # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.IncrementalPCA.html
+            latent_model = IncrementalPCA(n_components=latent_dim)
+            total_time = 0
+            for i in range(n_training_batches):
+                # Get a training batch of rescaled image vectors
+                training_batch_rescaled = training_set_rescaled[i * batch_size : (i + 1) * batch_size]
+                
+                # Partial fit Incremental PCA to the rescaled training batch
+                tic = time.time()
+                latent_model.partial_fit(training_batch_rescaled)
+                toc = time.time()
+                print("It takes {:.2f} seconds for Incremental PCA to fit to a training batch of shape {}.".format(toc-tic, training_batch_rescaled.shape))
+                total_time += toc - tic
+            
+            print("It takes {:.2f} seconds total for Incremental PCA to fit to the training set of shape {}.".format(total_time, training_set_rescaled.shape))
+        
         else:
             raise Exception("Unsupported latent method. Please provide one of the following: principal_component_analysis, diffusion_map")
         
@@ -74,16 +103,26 @@ def build_latent_space(dataset_file, image_type, target_type, latent_method, lat
         # Create a new dataset for the latent vectors
         latent_vectors_handle = dataset_file_handle.create_dataset(latent_method, latent_vectors_shape, dtype='f')
 
-        # Apply latent method to batches of images in the dataset and insert the resulting latent vectors into the dataset
-        n_batches = dataset_size // batch_size
+        # Apply latent method to batches of images in the dataset and add the resulting latent vectors into the dataset
+        total_time = 0
         for i in range(n_batches):
+            # Get a batch of images
             image_batch = images_handle[i * batch_size : (i + 1) * batch_size]
+            
+            # Reshape into image vectors
             image_batch_vectors = image_batch.reshape(batch_size, h * w)
+            
+            # Rescale between 0 and 1
             image_batch_vectors_rescaled = minmax_rescale(image_batch_vectors)
             
+            # Transform the rescaled image vectors using the fit latent method into latent vectors
             tic = time.time()
             latent_vectors = latent_model.transform(image_batch_vectors_rescaled)
             toc = time.time()
-            print("It takes {:.2f} seconds for transforming a batch of {} images.".format(toc-tic, image_batch_vectors_rescaled.shape))
+            print("It takes {:.2f} seconds for transforming a batch of {} image vectors.".format(toc-tic, image_batch_vectors_rescaled.shape))
+            total_time += toc - tic
             
+            # Add the latent vectors into the dataset
             latent_vectors_handle[i * batch_size : (i + 1) * batch_size] = latent_vectors
+        
+        print("It takes {:.2f} seconds total to transform the dataset of shape {}.".format(total_time, (dataset_size, h, w)))
